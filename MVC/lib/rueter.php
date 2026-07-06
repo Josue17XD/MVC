a4212fbad2091656e1fb8d5848a6eb486d1249e2
Josue17XD <kikesasukedeleon@gmail.com>
2026-07-06 11:51:26 -0600

    Add files via upload

---
+++ MVC/lib/rueter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace lib;
+
+class ruete{
+    private $ruetes = [
+
+        'GET' => [
+            'contact' => function(){
+
+        },
+        'about' => function(){
+
+        },
+        ],
+
+        'POST' => [
+
+        ],
+      
+    ];
+
+    public function get(){
+
+    }
+
+    public function post(){
+
+    }
+    
+}
